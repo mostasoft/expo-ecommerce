bd9d68fa227853455987386c4306075586382912
mostakin <mostasoft@gmail.com>
2026-02-06 11:01:54 +0600

    add root package.json for render deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,25 @@
-import express from "express"
+import express from "express";
+import path from "path";
+import { ENV } from "./config/env.js";
 
-const app = express()
+const app = express();
+const __dirname = path.resolve();
 
+app.get("/api/health", (req, res) => {
+  res.status(200).send("server is healthy");
+});
 
-app.get("/api/health", (req, res)=>{
-  res.status(200).send("server is healthy")
-})
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
 
-app.listen(3000, ()=>console.log("server is running on 3000"))
+  // SPA fallback route (MUST be last)
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../admin", "dist", "index.html")
+    );
+  });
+}
+
+app.listen(ENV.PORT, () =>
+  console.log("server is running on port", ENV.PORT)
+);
